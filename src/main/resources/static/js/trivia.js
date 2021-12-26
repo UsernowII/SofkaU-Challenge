940251ded252d0d177aca2146c804c48e398bdf2
Usernow <usernow.82@gmail.com>
2021-12-26 09:46:46 -0500

    Next question or close game

--- src/main/resources/static/js/trivia.js
+++ src/main/resources/static/js/trivia.js
@@ -92,8 +92,27 @@ function btnAction(index){
     if(posibleQuestions[index] === currentQuestion.answer){
         selectQuestionRandom();
     }else{
-        alert("respuesta equivocada")
+        alert("Lo siento! ..repuesta incorrecta","danger")
+        //setTimeout(back , 1000);
+
     }
+}
+
+function alert(message, type) {
+    var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+    var wrapper = document.createElement('div')
+
+    wrapper.innerHTML = '<div class="alert alert-' + type + ' form-control '+' role="alert">'
+        + message + '</div>'
+    /*wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">'
+        + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' +
+        '</div>'*/
+    alertPlaceholder.append(wrapper)
+
+}
+
+function back(){
+    location.reload();
 }
 
 
